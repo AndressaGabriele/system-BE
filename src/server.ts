ae9f9e7f1429aacefdff5693175b8f10b8c4ae3d
AndressaGabriele <andressa.chagas79@gmail.com>
2024-03-04 19:10:25 -0300

    update be

--- src/server.ts
+++ src/server.ts
@@ -10,23 +10,6 @@ app.setErrorHandler((error, request, reply) => {
     reply.code(400).send({ message: error.message })
 })
 
-
-// const start = async () => {
-
-//     await app.register(cors)
-//     await app.register(routes)
-
-//     try{
-//         const PORT = Number(process.env.PORT)
-//         app.listen({
-//             port: PORT 
-//         })
-//     }catch(err){
-//         process.exit(1) 
-//     }
-// }
-
-// start();
 app.register(cors)
 app.register(routes)
 
