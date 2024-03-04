a51dda2de551a41bd4b4acc3fc43f3c766d50c73
AndressaGabriele <andressa.chagas79@gmail.com>
2024-03-04 18:51:11 -0300

    update be

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import cors from "@fastify/cors";
 import { error } from "console";
 import { request } from "http";
 
-const app = fastify({ logger: true })
+const app = fastify()
 
 app.setErrorHandler((error, request, reply) => {
     reply.code(400).send({ message: error.message })
@@ -33,7 +33,7 @@ app.register(routes)
 try {
     const PORT = Number(process.env.PORT)
     app.listen({
-        port: PORT
+        port: PORT, host: '0.0.0.0'
     })
 } catch (err) {
     process.exit(1)
